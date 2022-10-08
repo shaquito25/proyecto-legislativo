91c6b44955612a78def6a427b164783aded75984
arturo <arturogabriel25@gmail.com>
2022-10-08 10:34:26 -0400

    ajustes visuales

--- app/models/perfil.rb
+++ app/models/perfil.rb
@@ -1,3 +1,12 @@
 class Perfil < ApplicationRecord
     has_many :users
+    has_many :expedients
+
+    def isAdmin?
+        codigo =="admin"
+    end
+
+    def isInv?
+        codigo =="inv"
+    end
 end
